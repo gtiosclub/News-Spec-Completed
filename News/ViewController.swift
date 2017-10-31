2b03adc11d16c195462cbbe6d22e5c58d75ca1e2
Cal Stephens <cal@calstephens.tech>
2017-10-31 13:23:10 -0400

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Cal Stephens on 10/31/17.
+//  Copyright © 2017 GT iOS Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
